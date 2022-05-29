9096c74e3a4369ecdc9df7f4af3ffab64c47c819
Lut Dinar Fadila <lutdinarfadila10@gmail.com>
2022-05-29 23:47:37 +0700

    latihan logika pengulangan dan pengkondisian

--- index.js
+++ index.js
@@ -24,18 +24,32 @@ function lakukanLooping(arrPegawai) {
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
   let hasilLooping = null;
+  hasilLooping = [];
+  dataYangAkanDilooping.forEach(pegawai => {
+    hasilLooping.push(pegawai.namaDepan + " " + pegawai.namaBelakang);
+  });
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+  dataYangAkanDilooping.forEach(pegawai => {
+    if (pegawai.jenisKelamin === "M") {
+      jumlahPria += 1;
+    }
+  });
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+  dataYangAkanDilooping.forEach(pegawai => {
+    if (pegawai.jenisKelamin === "F") {
+      jumlahWanita += 1;
+    }
+  });
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +63,15 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria == jumlahWanita) {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  } else {
+    if (jumlahPria > jumlahWanita) {
+      komentar = "Jumlah Pria lebih banyak dari Wanita";
+    } else {
+      komentar = "Jumlah Wanita lebih banyak dari Pria";
+    }
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +88,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
